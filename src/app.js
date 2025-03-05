f8e7aaa0a8b429ad4794a758d6d83cc580fc9f55
raulbordon <raulbordon2002@hotmail.es>
2025-03-05 09:58:18 +0000

    genraedor de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  const suits = ['♠', '♥', '♦', '♣'];
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let value = values[Math.floor(Math.random() * values.length)];
+  
+  document.getElementById("top").textContent = value;
+  document.getElementById("suit").textContent = suit;
+  document.getElementById("bottom").textContent = value;
+  
+  if (suit === '♥' || suit === '♦') {
+      document.getElementById("card").style.color = "red";
+  } else {
+      document.getElementById("card").style.color = "black";
+  }
+}
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = generateCard;
